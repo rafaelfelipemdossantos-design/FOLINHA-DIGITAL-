71567f3a7032f3386127f07ef0df8d3c101bb4eb
rafaelfelipemdossantos-design <rafael_felipe.m.dos.santos@hotmail.com>
2026-07-04 19:27:28 -0300

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'folinha-digital-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
